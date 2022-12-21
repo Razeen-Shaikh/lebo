3f3967ec0e1c036028faa073ac49698db496d16e
Razeen Shaikh <34880700+Razeen-Shaikh@users.noreply.github.com>
2022-12-21 16:26:45 +0000

    added statistics page and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
+import { StatisticsComponent } from './statistics/statistics.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: "", component: LandingPageComponent }];
+const routes: Routes = [{ path: "", component: LandingPageComponent }, { path: "statistics", component: StatisticsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    StatisticsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
